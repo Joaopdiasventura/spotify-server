cf2b9b95d14f608e01918dd2661c68337ad5e99b
João Paulo Dias <joaopdias.dev@gmail.com>
2025-10-12 21:15:58 -0300

    ci: add a handler to use the app on vercel

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { static as Static } from "express";
 import { join } from "node:path";
 import { AppModule } from "./app.module";
+import { Express, Request, Response } from "express";
 
-async function bootstrap(): Promise<void> {
+let server: Express;
+
+async function bootstrap(): Promise<Express> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
@@ -27,6 +30,14 @@ async function bootstrap(): Promise<void> {
   if (configService.get<string>("env") != "production")
     app.use("/uploads", Static(join(__dirname, "..", "uploads")));
 
-  await app.listen(configService.get<number>("port")!);
+  await app.init();
+  return app.getHttpAdapter().getInstance();
 }
-bootstrap();
+
+export default async function handler(
+  req: Request,
+  res: Response,
+): Promise<unknown> {
+  if (!server) server = await bootstrap();
+  return server(req, res);
+}

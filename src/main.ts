8c8e5f586fae5f5b12edc3f2599a725a4369f400
João Paulo Dias <joaopdias.dev@gmail.com>
2025-10-12 22:20:02 -0300

    fix: add the vercel handler

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,41 @@
-import { NestFactory } from "@nestjs/core";
-import { ConfigService } from "@nestjs/config";
-import { ValidationPipe } from "@nestjs/common";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { static as Static } from "express";
-import { join } from "node:path";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { static as Static } from 'express'
+import { join } from 'node:path'
+import { AppModule } from './app.module'
+import type { Express, Request, Response } from 'express'
 
-async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
+let server: Express
+
+async function bootstrap(): Promise<Express> {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const configService = app.get(ConfigService)
 
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
+      transformOptions: { enableImplicitConversion: true }
+    })
+  )
 
   app.enableCors({
-    origin: configService.get("client.url"),
-    methods: ["GET", "DELETE", "POST", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  });
+    origin: configService.get('client.url'),
+    methods: ['GET', 'DELETE', 'POST', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
 
-  if (configService.get<string>("env") != "production")
-    app.use("/uploads", Static(join(__dirname, "..", "uploads")));
+  if (configService.get<string>('env') != 'production')
+    app.use('/uploads', Static(join(__dirname, '..', 'uploads')))
 
-  await app.listen(configService.get<number>("port")!);
+  await app.init()
+  await app.listen(configService.get<number>('port')!)
+  return app.getHttpAdapter().getInstance()
 }
-bootstrap();
+
+export default async function handler(req: Request, res: Response): Promise<unknown> {
+  if (!server) server = await bootstrap()
+  return server(req, res)
+}
